7ded6d2bfc6508354244728f190db398cb5ec344
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-04-26 22:01:48 +0800

    基础代码写完

---
+++ EditWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleBookkeeping
+{
+    /// <summary>
+    /// EditWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        public BookkeepingEntry Entry { get; private set; }
+
+        public EditWindow()
+        {
+            InitializeComponent();
+            Entry = new BookkeepingEntry();
+            Entry.Date = DateTime.Today;
+            DataContext = Entry;
+        }
+
+        private void OnOKClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void OnCancelClicked(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SimpleBookkeeping
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<BookkeepingEntry> bookkeepingEntries = new List<BookkeepingEntry>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadDataFromJson();
+            dataGrid.ItemsSource = bookkeepingEntries;
+        }
+
+        private void OnAddClicked(object sender, RoutedEventArgs e)
+        {
+            EditWindow editWindow = new EditWindow();
+            if (editWindow.ShowDialog() == true)
+            {
+                bookkeepingEntries.Add(editWindow.Entry);
+                SaveDataToJson();
+            }
+        }
+
+        private void OnDeleteClicked(object sender, RoutedEventArgs e)
+        {
+            BookkeepingEntry entry = dataGrid.SelectedItem as BookkeepingEntry;
+            if (entry != null)
+            {
+                bookkeepingEntries.Remove(entry);
+                SaveDataToJson();
+            }
+        }
+
+        private void OnImportClicked(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string jsonString = File.ReadAllText(openFileDialog.FileName);
+                try
+                {
+                    JArray jsonArray = JArray.Parse(jsonString);
+                    bookkeepingEntries = jsonArray.ToObject<List<BookkeepingEntry>>();
+                    dataGrid.ItemsSource = bookkeepingEntries;
+                    SaveDataToJson();
+                }
+                catch (JsonReaderException ex)
+                {
+                    MessageBox.Show("Invalid JSON format: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void OnExportClicked(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, JsonConvert.SerializeObject(bookkeepingEntries, Formatting.Indented));
+            }
+        }
+
+        private void SaveDataToJson()
+        {
+            string jsonString = JsonConvert.SerializeObject(bookkeepingEntries, Formatting.Indented);
+            File.WriteAllText("bookkeeping.json", jsonString);
+        }
+
+        private void ReadDataFromJson()
+        {
+            if (File.Exists("bookkeeping.json"))
+            {
+                string jsonString = File.ReadAllText("bookkeeping.json");
+                try
+                {
+                    JArray jsonArray = JArray.Parse(jsonString);
+                    bookkeepingEntries = jsonArray.ToObject<List<BookkeepingEntry>>();
+                }
+                catch (JsonReaderException ex)
+                {
+                    MessageBox.Show("Invalid JSON format: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+
+    public class BookkeepingEntry
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+    }
+}
